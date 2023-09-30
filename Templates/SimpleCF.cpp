df230a1c2635c2cfa3eac7e43c6bb7c687be7bad
Andrew Liu <105452729+aliu05@users.noreply.github.com>
2023-09-30 11:55:25 +0800

    Create SimpleCF.cpp

---
+++ Templates/SimpleCF.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl "\n"
+
+
+
+void run_case() {
+    
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t; cin >> t;
+    while(t--)
+        run_case();
+    return 0;
+}
