1e2db229b90cb90d29dc3aba82312f2c3b1289d2
Andrew Liu <105452729+aliu05@users.noreply.github.com>
2023-09-30 11:58:18 +0800

    Update B.cpp

--- Codeforces/Codeforces Round 898 (Div. 4)/B.cpp
+++ Codeforces/Codeforces Round 898 (Div. 4)/B.cpp
@@ -4,23 +4,26 @@ using namespace std;
 #define ll long long
 #define nl "\n"
 
-int n;
+int n, a[10], mx, ans;
 
-void run_case() {
+void solve() {
     cin >> n;
-    int last = 1;
-    for(int i = 0; i < n; ++i) {
-        cout << last << ' ';
-        last += 2;
-    }
-    cout << nl;
+    ans = 1;
+    for(int i = 0; i < n; ++i)
+        cin >> a[i];
+    sort(a, a+n);
+    ++a[0];
+    for(int i = 0; i < n; ++i)
+        ans *= a[i];
+    cout << ans << nl;
 }
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
+    
     int t; cin >> t;
     while(t--)
-        run_case();
+        solve();
     return 0;
 }
