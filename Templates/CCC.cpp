c4753576fb8f414a6bdbf4bb69e17057e18978b3
Andrew Liu <105452729+aliu05@users.noreply.github.com>
2023-10-13 01:17:21 -0400

    Create CCC.cpp

---
+++ Templates/CCC.cpp
@@ -0,0 +1,19 @@
+/*
+    Complexity - O()
+
+    ...
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl "\n"
+
+
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    return 0;
+}
