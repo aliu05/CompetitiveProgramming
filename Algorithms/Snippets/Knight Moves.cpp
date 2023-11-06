23e6c85d4ec074aaeb4a2dc31840e79c9b8d533f
Andrew Liu <105452729+aliu05@users.noreply.github.com>
2023-11-06 16:43:53 +0800

    Create Knight Moves.cpp

---
+++ Algorithms/Snippets/Knight Moves.cpp
@@ -0,0 +1,7 @@
+/*
+    Description: On 2D matrix (board) list all 8 possible displacements for a knight.
+    Source: https://www.geeksforgeeks.org/possible-moves-knight/
+*/
+
+int X[8] = {2, 1, -1, -2, -2, -1, 1, 2};
+int Y[8] = {1, 2, 2, 1, -1, -2, -2, -1};
